ac6f8cd89a6539ca3252881a58b9fb197180a180
wangxiao <wangxiao1@u51.com>
2017-06-02 16:02:55 +0800

    [MDF]remove unseless import

--- app/src/main/java/cf/qishui/sodnotelean/ui/HomeCtlr.java
+++ app/src/main/java/cf/qishui/sodnotelean/ui/HomeCtlr.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bluelinelabs.conductor.Controller;
 import com.bluelinelabs.conductor.RouterTransaction;
 import com.bluelinelabs.conductor.changehandler.FadeChangeHandler;
 import com.bluelinelabs.conductor.rxlifecycle.ControllerEvent;
--- app/src/main/java/cf/qishui/sodnotelean/ui/NoteContentCtlr.java
+++ app/src/main/java/cf/qishui/sodnotelean/ui/NoteContentCtlr.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.bluelinelabs.conductor.Controller;
 import com.bluelinelabs.conductor.rxlifecycle.ControllerEvent;
 import com.bluelinelabs.conductor.rxlifecycle.RxController;
 
